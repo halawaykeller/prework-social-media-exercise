2a3e1c07cb9ce2b5792e309b0bc7d885c5bd540c
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-10 22:49:06 -0600

    added the rest of the social media files to project.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php // Example 26-4: index.php
-require_once 'header.php';
+  require_once 'header.php';
 
-echo "<br><span class='main'>Welcome to $appname,";
+  echo "<br><span class='main'>Welcome to $appname,";
 
-if ($loggedin) echo " $user, you are logged in.";
-else           echo ' please sign up and/or log in to join in.';
+  if ($loggedin) echo " $user, you are logged in.";
+  else           echo ' please sign up and/or log in to join in.';
 ?>
 
-</span><br><br>
-</body>
+    </span><br><br>
+  </body>
 </html>
